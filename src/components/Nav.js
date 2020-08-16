fd341780fb25904c019ce8ff9c2b544f53cd09ec
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-16 11:05:26 -0500

    Customized navbar with hover effects.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,19 +5,19 @@ const Nav = () => {
   return (
     <div id='navigation'>
       <ul>
-        <li>
+        <li data-text='Home'>
           <NavLink to='/home'>Home</NavLink>
         </li>
-        <li>
+        <li data-text='About'>
           <NavLink to='/about'>About</NavLink>
         </li>
-        <li>
+        <li data-text='Services'>
           <NavLink to='/services'>Services</NavLink>
         </li>
-        <li>
+        <li data-text='Portfolio'>
           <NavLink to='/portfolio'>Portfolio</NavLink>
         </li>
-        <li>
+        <li data-text='Contact'>
           <NavLink to='/contact'>Contact</NavLink>
         </li>
       </ul>
